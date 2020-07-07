26676fc684da8dad2e3df13ac023cb0b63fa7adc
Luvumbu <luvumbu.n@gmail.com>
2020-07-08 00:34:54 +0200

    ok

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head id="head">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>{{title}}</title>
+</head>
+<body>
+<?php 
+	include("app.php");
+	include("link.php");
+?>
+<style>
+	.w50p{
+		width: 70%;
+		margin: auto;
+		margin-top: 100px;
+	}
+	@media screen and (max-width: 1280px)
+{
+	.w50p{
+		width: 90%;
+		margin-top:50px;
+	}
+}
+	.fa-exclamation-triangle {
+		color:#f2a1b2;
+		font-size: 2em;
+		padding-left: 50px;
+	} 
+th {
+background-color: #d8e8dd;
+margin: 1px;
+
+padding: 20px;
+}
+th,td {
+	border:1px solid rgba(0, 0, 0, 0.2);
+}
+td {
+	padding: 10px;
+ 
+}
+</style>
+</body>
+</html>
---
+++ vue.js
@@ -0,0 +1,81 @@
+var head = new Vue({
+  el: '#head',
+  data: {    
+    title:"Bokonzi"
+  }
+})
+
+var header = new Vue({
+  el: '#header',
+  data: {
+    
+    
+  }
+})
+
+var section = new Vue({
+  el: '#section',
+  data: {
+    w50p:"w50p",
+    key_time_rest:"",    
+    key_time_end:"",
+    key_time_begin:"",    
+    key_total_time:"",
+    
+    info_time_rest:false,
+    info_time_end:false,
+    info_time_begin:false,
+    result:""
+  } , 
+  methods: {
+    time_rest(event) {
+      this.key_time_rest= parseFloat(event.target.value) ;      
+      this.info_time_rest=true;
+      document.getElementById("exclamation_rest").className="";
+      if(this.info_time_begin==true && this.info_time_end==true){
+        
+        this.key_time_begin= parseFloat(this.key_time_begin) ;
+        this.key_time_end= parseFloat(this.key_time_end) ;
+        this.result=this.key_time_end-this.key_time_begin-this.key_time_rest;
+        if(this.result<1){
+          this.result=false;
+        }
+ 
+      
+      }
+  },
+  time_end(event) {
+    this.key_time_end= parseFloat(event.target.value) ;
+    this.info_time_end=true;
+    document.getElementById("exclamation_end").className="";
+    this.key_time_begin= parseFloat(this.key_time_begin) ;
+    this.key_time_end= parseFloat(this.key_time_end) ;
+    this.result=this.key_time_end-this.key_time_begin-this.key_time_rest;
+    if(this.result<0){
+      this.result=false;
+    }
+
+},
+time_begin(event) {
+  console.log(event.target.value) ; 
+  this.key_time_begin= parseFloat(event.target.value) ;
+  this.info_time_begin=true;
+  document.getElementById("exclamation_begin").className="";
+  this.key_time_begin= parseFloat(this.key_time_begin) ;
+  this.key_time_end= parseFloat(this.key_time_end) ;
+  this.result=this.key_time_end-this.key_time_begin-this.key_time_rest;
+  if(this.result<1){
+    this.result=false;
+  }
+}
+
+}
+})
+
+var footer = new Vue({
+  el: '#footer',
+  data: {
+    
+  }
+})
+ 
